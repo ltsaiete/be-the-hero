001ac059a6ae05f2810ffc69e8dd82e2fe0a083e
Luis Saiete <luistsaiete@gmail.com>
2020-03-29 14:25:55 +0200

    Mobile da App

---
+++ mobile/App.js
@@ -0,0 +1,14 @@
+import 'intl'
+import 'intl/locale-data/jsonp/pt-MZ';
+
+import React from 'react';
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Routes/>
+    
+  );
+}
+
